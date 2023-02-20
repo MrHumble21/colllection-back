37cc427c588f2b10e69d93e43fa608d28574c814
Abdulboriy  Malikov <77222496+MrHumble21@users.noreply.github.com>
2023-02-20 13:39:53 +0500

    Update constants.js

--- database/constants/constants.js
+++ database/constants/constants.js
@@ -1,4 +1,4 @@
 const connectionString =
-  "mongodb+srv://Abdulboriy:itransition@cluster0.mpywc.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://Abdulboriy:itransition@cluster0.mpywc.mongodb.net/?retryWrites=true&w=majority/collections";
 
 exports.connectionString = connectionString;
